1d1e22f874be7871fc3306cfbb6ac4e77c3c1116
Daniil Shcherbakov <Charel@bk.ru>
2022-04-19 09:00:53 +0300

    Сохраняем состояние в bandl используя SavedStateHandle

--- app/src/main/java/com/example/mvvmsavedstate/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmsavedstate/MainViewModel.kt
@@ -2,16 +2,21 @@ package com.example.mvvmsavedstate
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
-class MainViewModel: ViewModel() {
+class MainViewModel(
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
 
-    private val _squares = MediatorLiveData<Squares>()
+    private val _squares = savedStateHandle.getLiveData<Squares>(KEY_SQUARES)
     val  squares:LiveData<Squares> = _squares
 
     init {
-       generateSquares()
+        if (!savedStateHandle.contains(KEY_SQUARES)) {
+            savedStateHandle[KEY_SQUARES] =createSquares()
+        }
     }
 
     fun generateSquares(){
@@ -23,5 +28,9 @@ class MainViewModel: ViewModel() {
             size = Random.nextInt(5, 11),
             colorProducer = {-Random.nextInt(0xFFFFFF)}
         )
+    }
+
+    companion object{
+        const val KEY_SQUARES = "squares"
     }
 }
